692defb959c24a34f47cf98c0e8ff10ae4ce88a9
unknown <minh.huang96@gmail.com>
2019-08-06 15:18:39 -0700

    finished 4 event

--- js/index.js
+++ js/index.js
@@ -1,1 +1,35 @@
 // Your code goes here
+
+const mouseoverControl_colorChange= document.querySelectorAll('.mouseoverControl');
+mouseoverControl_colorChange.forEach(e=>{
+    e.addEventListener("mouseover",(e)=>{
+         e.target.style.color ="red";
+        
+    });
+    e.addEventListener("mouseout",(e)=>{
+        e.target.style.color ="black";
+   });
+});
+
+const keydownInput = document.querySelector('input');
+const keydown = document.getElementById('showKeydown');
+keydownInput.addEventListener('keydown', (e)=>{
+    keydown.textContent= ` ${e.code}`;
+});
+
+const countryBox = document.querySelector('.countries');
+countryBox.onscroll;
+
+
+const scaleImg = document.querySelectorAll('.smallImg');
+
+scaleImg.forEach((e)=>{
+    e.addEventListener("click",(e)=>{
+        e.src="img/cat.jpg";
+        
+    });
+    
+    
+});
+
+
